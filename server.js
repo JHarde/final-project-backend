ccf3693170b32c32955b20e03f4475b775faec01
LillanEriksson <nailsbylillan@hotmail.com>
2021-01-18 11:57:35 +0100

    Created endpoints for highscore and questions, and mongoose models for highscore and questions

--- server.js
+++ server.js
@@ -1,24 +1,61 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// list endpoints in the '/' route
+const listEndpoints = require('express-list-endpoints');
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Mongooose connection
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/8080';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Mongoose model for highscore
+const Highscore = new mongoose.model('Highscore', {
+	name: { type: String },
+	score: { type: Number },
+});
+
+// Mongoose model for questions
+const Questions = new mongoose.model('Questions', {
+	id: { type: Number },
+	question: {
+		type: String,
+		answers: [
+			{
+				id: { type: Number },
+				answer: { type: String },
+				isCorrect: { type: Boolean },
+			},
+		],
+	},
+});
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start defining your routes here
+// List endpoints
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+	res.send(listEndpoints(app));
+});
+
+app.get('/highscore', (req, res) => {
+	res.send('Here goes the highscore');
+});
+
+app.post('/highscore', (req, res) => {});
+
+app.get('/questions', (req, res) => {});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+	console.log(`Server running on http://localhost:${port}`);
+});
